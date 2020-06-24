f5c804f992b2c8a5be1a56d4841fa8f508423ba6
Ping Yang (Peter) <ping.yang@ibm.com>
2020-06-24 00:21:53 -0500

    Add new routes for get weather through coordinates

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -21,7 +21,6 @@ router.get('/geocode',function(req,res){
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
-
   try {
     let data = await fetch(url);
     let weather = await data.json();
@@ -41,7 +40,35 @@ router.post('/get_weather', async function (req,res) {
     console.log(err);
     res.render('index', {weather: null, error: 'Error: Unable to invoke OpenWeatherMap API'});
   }
+});
 
+router.post('/get_weather_coordinate', async function (req,res) {
+  var latitude = 40.75637123;
+  var longitue = -73.98545321;
+  console.log(latitude,longitue);
+  // let latitude = req.body.LON;
+  // let longitue = req.body.LAT;
+  let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitue}&units=${UNITS}&appid=${OWM_API_KEY}`;
+
+  try {
+    let data = await fetch(url);
+    let weather = await data.json();
+    console.log(weather);
+    if(weather.cod == '404' && weather.main == undefined) {
+      res.render('geocode', {weather: null, error: 'Error: Unknown city'});
+    }
+    else if (weather.cod == '401' && weather.main == undefined) {
+      res.render('geocode', {weather: null, error: 'Error: Invalid API Key. Please see http://openweathermap.org/faq#error401 for more info.'});
+    }
+    else {
+      let unit_hex = (UNITS == 'imperial') ? '&#8457' : '&#8451';
+      res.render('geocode', {weather: weather, error: null, units: unit_hex});
+    }
+  }
+  catch (err) {
+    console.log(err);
+    res.render('geocode', {weather: null, error: 'Error: Unable to invoke OpenWeatherMap API'});
+  }
 });
 
 module.exports = router;
